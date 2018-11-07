a96a0f957342036b1d50c59899b1582b4cc1c5d4
Michael Mimo Moratti <michael.moratti@flarm.com>
2018-11-07 08:10:01 +0100

    added quick sort of amount of flights by pilot

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import pickle
+import operator
 
 
 def main():
     with open('all_igcs.pickle', 'rb') as handle:
         data = pickle.load(handle)
 
+        pilots = dict()
+
         for igc in data:
+            if igc.pilot in pilots:
+                pilots[igc.pilot] += 1
+            else:
+                pilots[igc.pilot] = 1
             print('{} --> {}'.format(igc.file, igc.pilot))
+
+        for pilot, amount in sorted(pilots.items(), key=operator.itemgetter(1), reverse=True):
+            print('{} = {}'.format(pilot, amount))
 
 
 if __name__ == '__main__':
